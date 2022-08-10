390aa62cf1bb801dcf95e2b9442934fc39947f6c
unknown <stockjanitor@gmail.com>
2022-08-09 17:38:59 -0700

    start C class

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[5];
+    a[0]=12;
+    a[1]=15;
+    a[2]=25;
+
+    for(int x:a)
+    {
+        cout << x << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
