974b03777686336173ddcdf1db1adf2e878e73c6
unknown <stockjanitor@gmail.com>
2022-08-22 16:57:20 -0700

    ptrs and mallock

---
+++ 1_basics/main.cpp
@@ -0,0 +1,40 @@
+/* 
+Notes:
+
+defining strucutre, size of structure, declaring a structure, accessing members
+
+
+*/
+#include <stdio.h> // C
+#include<iostream> // C++
+#include <stdlib.h>
+using namespace std;
+
+// definition
+struct Rectangle{
+    int length;
+    int breadth;
+};
+
+int main()
+{
+
+    int *p;
+
+
+    // C language
+    p=(int *)malloc(5*sizeof(int)); // allocation of heap memory
+    free(p); // free it, 
+
+    // C++ langugage
+    p=new int [5];
+
+    p[0]=10; p[1]=15;p[2]=14;p[3]=21;p[4]=31;
+
+    for(int i=0;i<5;i++)
+    cout << p[i]<< endl;
+
+    delete [] p;
+
+
+}
